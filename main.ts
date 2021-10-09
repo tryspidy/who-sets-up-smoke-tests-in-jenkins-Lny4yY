f555ea6ea2d60cb29312be460dbfbbfde5ef582f
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-09 12:06:17 +0530

    Initial commit

---
+++ main.ts
@@ -0,0 +1,9 @@
+As an automation engineer, I work with the devops/operations supports to 
+create my smoke tests.
+ For running my tests, I need certain plugins installed on Jenkins, 
+   I also need java, maven configured on jenkins, I also need browsers 
+installed on the server where the tests will run.
+ In my project, only devops/operations support team members have right to 
+ do the configurations above. that is why I have to work with them.
+ Once the configuration of the Jenkins is completed, 
+  I can create and run the smoke test job.
